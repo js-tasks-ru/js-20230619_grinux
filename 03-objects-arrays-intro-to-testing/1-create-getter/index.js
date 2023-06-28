47ede46bc558cdd9bb722fc0972a1c247b4fe60a
grinux <g.korotov@gmail.com>
2023-06-29 03:26:56 +0400

    3-1 completed

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,29 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  path = path.split('.');
+  return function get_prop(obj) {
+    if (Object.keys(obj).length === 0)
+      return undefined;
+    let child = path.shift();
+    if (path.length)
+      return get_prop(obj[child]);
+    return obj[child];
+  };
 }
+  // let major_path;
+  // let dot = path.indexOf('.'); 
+  // if (dot)
+
+  // const product = {
+  //   category: {
+  //     title: "Goods"
+  //   }
+  // }
+
+  // const obj = { 
+  // };
+  
+  // const getter = createGetter('src');
+  
+  // console.log(getter(obj)); // Goods
