a9287ce213058e60c9cf421c4b7e0ac556c1592d
grinux <g.korotov@gmail.com>
2023-06-29 14:55:49 +0400

    3-2 completed

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,19 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (!obj)
+    return undefined;
+  const props = Object.entries(obj);
+  props.map(prop => {
+    [prop[0], prop[1]] = [prop[1], prop[0]];
+  });
+  return Object.fromEntries(props);
+}
 
+export function invertObj1(obj) {
+  return obj ?
+    Object
+      .fromEntries(Object
+        .entries(obj)
+        .map(([key, value]) => [value, key])) : undefined;
 }
