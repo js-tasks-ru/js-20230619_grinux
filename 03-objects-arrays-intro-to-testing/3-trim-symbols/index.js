377788b389292670c4e7d170d0d50c9beda8b4c9
grinux <g.korotov@gmail.com>
2023-06-30 00:52:17 +0400

    3-3 improvements

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,21 +4,18 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-
-//нужно ли явно проверять size === undefined, если тесты проходят, т.к при 
-//сравнении на 20й строке undefined преобразуется в NaN, и логика не нарушается?
 export function trimSymbols(string, size) {
-  //if (size === undefined) return string;
+  if (size === undefined) 
+    return string;
   let catch_char = '';
   let count = 0;
-  console.log(size);
   return [...string].filter(char => {
     if (catch_char != char)
     {
       catch_char = char;
       count = 1;
     }
-    return count++ > size ? false : true;
+    return count++ <= size;
   })
   .join('');
 }
