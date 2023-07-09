81338034daa0cd5a6cc4dadc73249b16e76cf487
grinux <g.korotov@gmail.com>
2023-07-09 17:58:29 +0400

    6-2 completed but have same trouble while testing on innerText content. Please help me!

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,7 +1,63 @@
 class Tooltip {
-  initialize () {
+  static #instance = null;
+  constructor() {
+    if (!Tooltip.#instance) 
+      Tooltip.#instance = this;
+    else 
+      return Tooltip.#instance;
+  }
 
+  initialize () {
+    this.element = this.create_element(`<div class="tooltip"></div>`);
+    this.element.style.position = 'absolute';  
+    const mv = this.move.bind(this);
+    document.addEventListener('pointerover', event => {
+      const src = event.target.closest('[data-tooltip]');
+      if (src)
+      { 
+        document.addEventListener('pointermove', mv);
+        document.addEventListener('pointerout', event => {
+          const src = event.target.closest('[data-tooltip]');
+          if (src)
+          { 
+            document.removeEventListener('pointermove', mv);
+            this.element.remove();
+            this.element.innerText = ''; 
+          }
+        }, {once: true});
+        
+        this.element.style.left = event.pageX + 10 + 'px';
+        this.element.style.top = event.pageY + 10 + 'px';
+        this.render(src.dataset.tooltip);
+      }
+    });
+  }
+
+  move(event) {
+    this.element.style.left = event.pageX + 10 + 'px';
+    this.element.style.top = event.pageY + 10 + 'px';
+  }
+
+  render(text = '') {
+    this.element.innerText = text;
+    document.body.appendChild(this.element);
+  }
+
+  create_element(html)
+  {
+    const div = document.createElement('div');
+    div.innerHTML = html;
+    return div.firstElementChild;
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
   }
 }
+
 
 export default Tooltip;
