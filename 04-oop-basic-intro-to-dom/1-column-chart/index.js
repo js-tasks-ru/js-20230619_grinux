cd47c90459d77fa1e8f8b6060206b6d1354a696d
grinux <g.korotov@gmail.com>
2023-07-11 13:58:27 +0400

    4-1 requested improvements

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -2,59 +2,67 @@ export default class ColumnChart {
   
   chartHeight = 50;
 
-  constructor(chart_data = {})
+  constructor({
+    data = [],
+    label = '',
+    link,
+    value,
+    formatHeading
+  } = {})
+
   {
-    this.build(chart_data);
-    this.update(chart_data.data);
+    this.data = data;
+    this.label = label;
+    this.link = link;
+    this.fmt = formatHeading;
+    this.value = value;
+
+    this.build(this.chart_data);
+    this.update(this.data);
   }
 
-  build(chart_data)
+  build()
   {
-    this.element = this.create_element(`
-      <div class="column-chart column-chart_loading" style="--chart-height: 50">
-        <div class="column-chart__title"></div>
+    this.element = this.templeate();
+    this.chart = this.element.querySelector('.column-chart__chart');
+  }
+
+  templeate() {
+    return this.create_element (`
+      <div class="column-chart column-chart_loading" style="--chart-height: ${this.chartHeight}">
+        <div class="column-chart__title">
+          ${this.label}
+          ${this.link ? `<a href="/${this.link}" class="column-chart__link">View all</a>` : ''}
+        </div>
         <div class="column-chart__container">
-          <div data-element="header" class="column-chart__header"></div>
+          <div data-element="header" class="column-chart__header">
+            ${this.value ? 
+              this.fmt ? this.fmt([this.value.toLocaleString("en-US")]) : this.value : 
+              ''}
+          </div>
           <div data-element="body" class="column-chart__chart"></div>
         </div>    
       </div>
-    `);
-    this.chart = this.element.querySelector('.column-chart__chart');
-    this.chart_data = chart_data;
-    if (this.chart_data.label)
-      this.element.querySelector('.column-chart__title').textContent = this.chart_data.label;
-    if (this.chart_data.value) {
-      if (this.chart_data.formatHeading)
-        this.chart_data.value = this.chart_data
-          .formatHeading([this.chart_data.value.toLocaleString("en-US")])
-      this.element.querySelector('.column-chart__header').textContent = this.chart_data.value;
-    }
-    if (this.chart_data.link)
-      this.element
-        .querySelector('.column-chart__title')
-        .insertAdjacentHTML('beforeend', `
-        <a href="/${this.chart_data.link}" class="column-chart__link">View all</a>
-        `);
+  `);
   }
 
   update(data = []) {
-    this.chart_data.data = data;
+    this.data = data;
     this.chart.innerHTML = '';
-    if (this.chart_data.data.length)
-    {
-      const max_data = Math.max(...this.chart_data.data);
-      for (let data_val of this.chart_data.data) {
-        const value = Math.floor(data_val * this.chartHeight / max_data);
-        const percnt = Math.round(data_val * 100 / max_data);
-        this.chart.appendChild(this.create_element(`
+    if (!this.data.length) {
+      this.element.classList.add('column-chart_loading');
+      return;
+    }
+    
+    const max_data = Math.max(...this.data);
+    for (let data_val of this.data) {
+      const value = Math.floor(data_val * this.chartHeight / max_data);
+      const percnt = Math.round(data_val  * 100 / max_data);
+      this.chart.appendChild(this.create_element(`
         <div style="--value: ${value}" data-tooltip="${percnt}%"></div>
         `));
-      }
-      this.element.classList.remove('column-chart_loading');
     }
-    else {
-      this.element.classList.add('column-chart_loading');
-    }
+    this.element.classList.remove('column-chart_loading');
   }
 
   create_element(html) {
