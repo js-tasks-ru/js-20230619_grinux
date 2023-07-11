8856bacc1ab7826fc0818ae3d58cb6d94cbe5995
grinux <g.korotov@gmail.com>
2023-07-12 00:52:08 +0400

    6-2 fixes

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -9,30 +9,36 @@ class Tooltip {
 
   initialize () {
     this.element = this.create_element(`<div class="tooltip"></div>`);
-    this.element.style.position = 'absolute';  
-    const mv = this.move.bind(this);
-    document.addEventListener('pointerover', event => {
-      const src = event.target.closest('[data-tooltip]');
-      if (src)
-      { 
-        document.addEventListener('pointermove', mv);
-        this.element.style.left = event.pageX + 10 + 'px';
-        this.element.style.top = event.pageY + 10 + 'px';
-        this.render(src.dataset.tooltip);
-      }
-    });
-    document.addEventListener('pointerout', event => {
-      const src = event.target.closest('[data-tooltip]');
-      if (src)
-      { 
-        document.removeEventListener('pointermove', mv);
-        this.element.remove();
-        this.element.textContent = ''; 
-      }
-    });
+    this.element.style.position = 'absolute';
+
+    this.p_move = this.p_move.bind(this);
+    this.p_over = this.p_over.bind(this);
+    this.p_out = this.p_out.bind(this);
+
+    document.addEventListener('pointerover', this.p_over);
+    document.addEventListener('pointerout', this.p_out);
   }
 
-  move(event) {
+  p_over(event) {
+    const src = event.target.closest('[data-tooltip]');
+    if (src) {
+      document.addEventListener('pointermove', this.p_move);
+      this.element.style.left = event.pageX + 10 + 'px';
+      this.element.style.top = event.pageY + 10 + 'px';
+      this.render(src.dataset.tooltip);
+    }
+  }
+
+  p_out(event) {
+    const src = event.target.closest('[data-tooltip]');
+    if (src) {
+      document.removeEventListener('pointermove', this.p_move);
+      this.element.remove();
+      this.element.textContent = '';
+    }
+  }
+
+  p_move(event) {
     this.element.style.left = event.pageX + 10 + 'px';
     this.element.style.top = event.pageY + 10 + 'px';
   }
@@ -54,6 +60,9 @@ class Tooltip {
   }
 
   destroy() {
+    document.removeEventListener('pointerover', this.p_over);
+    document.removeEventListener('pointerout', this.p_out);
+    document.removeEventListener('pointermove', this.p_move);
     this.remove();
   }
 }
