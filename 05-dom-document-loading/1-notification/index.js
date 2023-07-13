106a67ca417e2dcab2b3413b81b01623b2cb55f8
grinux <g.korotov@gmail.com>
2023-07-13 17:53:40 +0400

    5-1 requested improvements

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,43 +1,44 @@
 export default class NotificationMessage {
   static #instance = null;
+  
+  defaultDuration = 50;
 
   constructor(title = '', {
-    duration = 50,
+    duration,
     type = ''
   } = {}) {
-
 
     this.duration = Math.max(50, duration);
     this.title = title;
     this.type = type;
 
     this.build();
-    this.set_opts();
   }
 
   build() {
-    this.element = this.create_element(`
-    <div class="notification" style="--value:20s">
-      ${this.title}
-      <div class="timer"></div>
-      <div class="inner-wrapper">
-        <div class="notification-header"></div>
-        <div class="notification-body">${this.title}</div>
+    this.element = this.createElement(this.getTemplate());
+    this.element.style.setProperty('--value',this.duration / 1000 + 's');
+    if (this.type)
+    {
+      this.element.classList.add(this.type);
+      this.element.querySelector('.notification-header').textContent = this.type;
+    }
+  }
+
+  getTemplate() {
+    return `
+      <div class="notification" style="--value:20s">
+        ${this.title}
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header"></div>
+          <div class="notification-body">${this.title}</div>
+        </div>
       </div>
-    </div>
-  `);
+  `;
   }
 
-  set_opts() {
-      this.element.style.setProperty('--value',this.duration / 1000 + 's');
-      if (this.type)
-      {
-        this.element.classList.add(this.type);
-        this.element.querySelector('.notification-header').textContent = this.type;
-      }
-  }
-
-  create_element(html) {
+  createElement(html) {
     const div = document.createElement('div');
     div.innerHTML = html;
     return div.firstElementChild;
@@ -65,6 +66,6 @@ export default class NotificationMessage {
 
     setTimeout(() => {
       this.remove();
-    }, this.duration - 50); //если тут сделать меньшую поправку, то окно
-  }                         //успевает моргнуть. Как правильно?
+    }, this.duration);
+  }                        
 }
