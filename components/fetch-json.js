158eb52dc65ca832f3f5187f34552c6af70c8a68
grinux <grinux@mail.ru>
2023-07-29 13:53:46 +0400

    fetchObserver implementation

--- components/fetch-json.js
+++ components/fetch-json.js
@@ -5,11 +5,15 @@
 export default async function(url, params) {
   let response;
 
+  const fetchObserver = new FetchObserver();
+  fetchObserver.fetchStart(); 
+
   try {
     // TODO: "toString" call needed for correct work of "jest-fetch-mock"
     response = await fetch(url.toString(), params);
   } catch (err) {
-    throw new FetchError(response, "Network error has occurred.");
+    fetchObserver.fetchEnd(); 
+    throw new FetchError(response, null, "Network error has occurred.");
   }
 
   let body;
@@ -23,6 +27,8 @@ export default async function(url, params) {
       errorText = (body.error && body.error.message) || (body.data && body.data.error && body.data.error.message) || errorText;
     } catch (error) { /* ignore failed body */ }
 
+    fetchObserver.fetchEnd(); 
+
     let message = `Error ${response.status}: ${errorText}`;
 
     throw new FetchError(response, body, message);
@@ -32,6 +38,8 @@ export default async function(url, params) {
     return await response.json();
   } catch (err) {
     throw new FetchError(response, null, err.message);
+  } finally {
+    fetchObserver.fetchEnd(); 
   }
 }
 
@@ -42,13 +50,54 @@ export class FetchError extends Error {
     super(message);
     this.response = response;
     this.body = body;
+
+    document.dispatchEvent(new CustomEvent('fetch-error', {
+      bubbles: true,
+      detail: message
+    }));
+
   }
 }
 
 // handle uncaught failed fetch through
 window.addEventListener('unhandledrejection', event => {
   if (event.reason instanceof FetchError) {
+    document.dispatchEvent(new CustomEvent('fetch-error', {
+      bubbles: true,
+      detail: event.reason.message
+    }));
+
     alert(event.reason.message);
   }
 });
+
+class FetchObserver {
+  static #instance = null;
+  counter = 0;
+
+  constructor() {
+    if (!FetchObserver.#instance) 
+    FetchObserver.#instance = this;
+    else 
+      return FetchObserver.#instance; 
+  }
+
+  fetchStart() {
+    if (!this.counter)
+      document.dispatchEvent(new CustomEvent('fetch-active', {
+        bubbles: true
+      }));
+    this.counter++;
+  }
+
+  fetchEnd() {
+    if (!this.counter)
+      throw new Error('FetchObserver counter mismatch');
+    this.counter--;
+    if (!this.counter)
+      document.dispatchEvent(new CustomEvent('fetch-completed', {
+        bubbles: true
+      }));
+  }
+}
 
